1b4e95f596061a240d17a2bf6e09136bd4093af5
Levils114 <levils114@gmail.com>
2021-08-26 09:57:53 -0300

    feat: header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import {Container,
+        Header,
+        UserWrapper,
+        UserInfo,
+        UserAvatar,
+        User,
+        UserGreeting,
+        UserName} from "./styles";
 
 export default function Dashboard(){
     return(
         <Container>
-            <Title>eae man</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <UserAvatar source={{ uri: 'https://avatars.githubusercontent.com/u/31908348?v=4' }}/>
+
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Levi Siebra</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     );
 }
